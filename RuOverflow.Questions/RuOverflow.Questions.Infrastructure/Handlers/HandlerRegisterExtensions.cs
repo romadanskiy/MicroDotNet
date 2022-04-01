22ef34c7e1c9af396d0a40d8d632e46f4803a2bf
Ilshat Mukhamediarov <61285419+IlshatM@users.noreply.github.com>
2022-04-01 17:37:36 +0300

    MS-178 - cleanup

--- RuOverflow.Questions/RuOverflow.Questions.Infrastructure/Handlers/HandlerRegisterExtensions.cs
+++ RuOverflow.Questions/RuOverflow.Questions.Infrastructure/Handlers/HandlerRegisterExtensions.cs
@@ -8,18 +8,12 @@ public static class HandlerRegisterExtensions
         private static readonly Type[] AllHandlerTypes = new[]
             { typeof(IAsyncHandler<>), typeof(IAsyncHandler<,>), typeof(IHandler<>), typeof(IHandler<,>) };
 
-        /// <summary>
-        /// Зарегестрировать хендлеры
-        /// </summary>
         public static void RegisterHandlers(this IServiceCollection services, Assembly assembly)
         {
             Array.ForEach(AllHandlerTypes, x => services.AddHandlerFrom(assembly, x));
             services.AddDecoratorsFrom(assembly);
         }
 
-        /// <summary>
-        /// Добавить хендлеры из определенной сборки
-        /// </summary>
         private static void AddHandlerFrom(this IServiceCollection services, Assembly assembly,
             Type openGenericInterfaceType, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
         {
@@ -32,9 +26,6 @@ public static class HandlerRegisterExtensions
                 .WithLifetime(serviceLifetime));
         }
 
-        /// <summary>
-        /// Добавить декораторы
-        /// </summary>
         private static void AddDecoratorsFrom(this IServiceCollection services, Assembly assembly)
         {
             var decorators = GetAllDecoratorTypesFromAssemblyInCorrectOrder(assembly);
@@ -52,9 +43,6 @@ public static class HandlerRegisterExtensions
             }
         }
 
-        /// <summary>
-        /// Получить все декораторы
-        /// </summary>
         private static TypeInfo[] GetAllDecoratorTypesFromAssemblyInCorrectOrder(Assembly assembly)
         {
             var decorators = assembly.DefinedTypes
@@ -65,9 +53,6 @@ public static class HandlerRegisterExtensions
             return decorators;
         }
 
-        /// <summary>
-        /// Получить самый точный интерфейс хендлера с открытым дженериком
-        /// </summary>
         private static Type GetMostSpecificOpenGenericHandlerInterface(Type[] handlerInterfaces)
         {
             var openHandlerInterface =
@@ -75,10 +60,7 @@ public static class HandlerRegisterExtensions
 
             return openHandlerInterface;
         }
-
-        /// <summary>
-        /// Получить самый точный интерфейс хендлера с закрытым дженериком
-        /// </summary>
+        
         private static Type GetMostSpecificClosedGenericHandlerInterface(Type[] handlerInterfaces,
             Type openHandlerInterface)
         {
