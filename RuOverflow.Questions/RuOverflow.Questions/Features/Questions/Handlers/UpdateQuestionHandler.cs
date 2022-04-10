29000b7e8b636eb311541720a9c783b918f390cd
Danila <57109784+Dqnilq@users.noreply.github.com>
2022-04-11 01:44:13 +0300

    qsthandler

---
+++ RuOverflow.Questions/RuOverflow.Questions/Features/Questions/Handlers/UpdateQuestionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using RuOverflow.Questions.EF;
+using RuOverflow.Questions.Features.Questions.Models;
+using RuOverflow.Questions.Infrastructure.Handlers;
+
+namespace RuOverflow.Questions.Features.Questions.Handlers;
+
+public class UpdateQuestionHandler : IAsyncHandler<QuestionCommands.UpdateQuestionCommand, Question>
+{
+    private readonly IDbContextFactory<RuFlowDbContext> _contextFactory;
+
+    public UpdateQuestionHandler(IDbContextFactory<RuFlowDbContext> contextFactory)
+    {
+        _contextFactory = contextFactory;
+    }
+    public async Task<Question> Handle(Question input)
+    {
+        var context = await _contextFactory.CreateDbContextAsync();
+        context.Questions.Update(input);
+        await context.SaveChangesAsync();
+        var question = await context.Questions.Where(s => s.Id == input.Id).FirstOrDefaultAsync();
+        return question ?? throw new InvalidOperationException();
+    }
+
+    public Task<Question> Handle(QuestionCommands.UpdateQuestionCommand input)
+    {
+        throw new NotImplementedException();
+    }
+}
