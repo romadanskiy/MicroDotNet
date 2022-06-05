4128224b4db28ab0541653bd905d686700ac1d31
Ilshat Mukhamediarov <61285419+IlshatM@users.noreply.github.com>
2022-06-05 17:42:56 +0300

    QuestionSearch-draft

---
+++ RuOverflow.Questions/RuOverflow.Questions/Features/Questions/Models/QuestionElasticDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RuOverflow.Questions.Features.Questions.Models
+{
+    public class QuestionElasticDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int Rating { get; set; }
+        public string UserId { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Modified { get; set; }
+        public List<Guid> Tags { get; set; } = new();
+    }
+}
--- RuOverflow.Questions/RuOverflow.Questions/Features/Questions/QuestionQuery.cs
+++ RuOverflow.Questions/RuOverflow.Questions/Features/Questions/QuestionQuery.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Nest;
 using RuOverflow.Questions.Base;
 using RuOverflow.Questions.EF;
 using RuOverflow.Questions.Features.Questions.Models;
+using RuOverflow.Questions.Infrastructure.Extensions;
 
 namespace RuOverflow.Questions.Features.Questions;
 
@@ -16,11 +18,31 @@ public class QuestionQuery
         return context.Questions.Where(x => x.Id == id);
     }
 
-    public async Task<IQueryable<Question>> GetQuestions(
-        [Service] IDbContextFactory<RuFlowDbContext> contextFactory)
+    public async Task<List<Question>> GetQuestions(
+        [Service] IDbContextFactory<RuFlowDbContext> contextFactory, [Service] ElasticClient elasticClient)
     {
-        var context = await contextFactory.CreateDbContextAsync();
-        return context.Questions.Select(s => s);
+        var documents = await elasticClient.SearchAsync<QuestionElasticDto>(
+            main => main.Query(
+                q =>
+                    SearchText(q) &&
+                    MatchUserId(q) &&
+                    q.MatchMany(f => f.Tags, new[] { "a792b957-7ec5-4eed-ad03-cb91c702b098" })
+            ));
     }
-    
+
+    private static QueryContainer MatchUserId(QueryContainerDescriptor<QuestionElasticDto> q)
+    {
+        return q.Match(s => s.Field(f => f.UserId).Query("1faa65e1-6daa-4935-b868-a89b7972345c"));
+    }
+
+    private static QueryContainer SearchText(QueryContainerDescriptor<QuestionElasticDto> q)
+    {
+        return q.MultiMatch(f => f
+            .Fields(x => x
+                .Field(e => e.Body).Field(e => e.Title, 2)
+            )
+            .Query("Как запустить docker?")
+            .Type(TextQueryType.Phrase)
+        );
+    }
 }
