431c3e66d60275db81bd540a5f6e4fee49051400
Ilnur Mukhametov <voda2600@mail.ru>
2022-06-21 01:41:19 +0300

    gRPC-updated

--- GqlUserService/build.gradle.kts
+++ GqlUserService/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -9,8 +12,9 @@ val mssqlDriverVersion: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.google.protobuf") version "0.8.18"
 }
 
 group = "com.example"
@@ -24,7 +28,22 @@ application {
 
 repositories {
     mavenCentral()
+    google()
+    maven {
+        url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+        name = "Ktor EAP"
+    }
 }
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/protobuf")
+        }
+    }
+}
+
+
 
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
@@ -37,13 +56,50 @@ dependencies {
     implementation("org.flywaydb:flyway-core:$flywayVersion")                                    //FlyWay
     implementation("com.zaxxer:HikariCP:$hikariCPVersion")                                     //HikariCP
     implementation("com.microsoft.sqlserver:mssql-jdbc:$mssqlDriverVersion")                     //MSSQL-Driver
-    implementation("org.postgresql:postgresql:42.3.4")                  //PostgreSQL
-    implementation("io.grpc:grpc-netty:1.46.0")
-    implementation("io.grpc:grpc-protobuf:1.39.0")
-    implementation("io.grpc:grpc-stub:1.46.0")
-    implementation("io.grpc:grpc-kotlin-stub:1.2.0")
-    implementation("com.google.protobuf:protobuf-kotlin:3.18.1")
-    implementation("com.google.protobuf:protobuf-java:3.18.1")
+    implementation("org.postgresql:postgresql:42.3.6")                  //PostgreSQL
+    implementation("com.google.protobuf:protobuf-kotlin:3.19.4")
+    api("io.grpc:grpc-protobuf:1.44.0")
+    api("com.google.protobuf:protobuf-java-util:3.19.4")
+    api("com.google.protobuf:protobuf-kotlin:3.19.4")
+    api("io.grpc:grpc-kotlin-stub:1.2.1")
+    api("io.grpc:grpc-stub:1.44.0")
+    runtimeOnly("io.grpc:grpc-netty:1.44.0")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.4"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.44.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.2.1:jdk7@jar"
+        }
+    }
+
+
+    generateProtoTasks {
+        generatedFilesBaseDir = "src"
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
 }
--- GqlUserService/src/main/kotlin/com/example/Application.kt
+++ GqlUserService/src/main/kotlin/com/example/Application.kt
@@ -4,9 +4,31 @@ import Database.DAOFacadeImpl
 import Database.initDatabase
 import io.ktor.application.*
 import com.example.plugins.*
+import io.grpc.ServerBuilder
+import registrationResult
 
-fun main(args: Array<String>): Unit =
+private class RegistrationService : RegistrationServiceGrpcKt.RegistrationServiceCoroutineImplBase() {
+    override suspend fun register(request: Register.User): Register.RegistrationResult {
+        print("Registering user ${request.lastname} ${request.firstname} ${request.middlename}, age: ${request.age}, gender: ${request.gender.name}")
+        return registrationResult { succeeded=true }
+    }
+}
+
+fun main(args: Array<String>): Unit{
     io.ktor.server.netty.EngineMain.main(args)
+    val port = System.getenv("PORT")?.toInt() ?: 50051
+    val server = ServerBuilder
+        .forPort(port)
+        .addService(RegistrationService())
+        .build()
+    server.start()
+    Runtime.getRuntime().addShutdownHook(Thread {
+        println("Shutdown server")
+        server.shutdown()
+    })
+    //wait for connection until shutdown
+    server.awaitTermination()
+}
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
